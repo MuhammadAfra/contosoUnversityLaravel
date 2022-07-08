4aac9d1323c8bebdf4e65ab9ae8666a5d878a6c8
MuhammadAfra <muhammadafra45@gmail.com>
2022-07-08 10:47:35 +0700

    Menambahkan file StudentController dan Model->Student

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::latest()->paginate(5);
+        return view('students.index', compact('student'))->with('i',(request()->input('page', 1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'LastName' => 'required',
+            'FirstMidName'=> 'required',
+            'EnrollmentDatae'=> 'required',
+        ]);
+
+        Student::create($request->all());
+
+        return redirect()->route('students.index')->with('succes','Data Berhasil di Input');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $students)
+    {
+        //
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $students)
+    {
+        //
+        return view('students.edit', compact('students'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $students)
+    {
+        //
+        $request->validate([
+            'LastName'=>'required',
+            'FirstMidName'=>'required',
+            'EnrollmentDate'=>'required',
+        ]);
+
+        $students->update($request->all());
+
+        return redirect()-route('students.index')-with('succes','Students Berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $students)
+    {
+        //
+        $students->delete();    
+
+        return redirect('student.index')->with('succes','Students berhasil dihapus');
+    }
+}
